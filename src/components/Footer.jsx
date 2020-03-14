60b144430866f2b6127b86d065d6784ecae223ee
ankit <ankitsinghwakefit@gmail.com>
2020-03-14 21:15:46 +0530

    responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "../App.css";
+import "bulma/css/bulma.css";
+
 
 function Footer(){
     return(
         <div className="footer-container">
             <div className="footer-logo"></div>
             <div className="footer-tagline">Presented by the Shri Mataji Nirmala Devi Sahaja Yoga World Foundation</div>
+            <button class="button is-white">White</button>
         </div>
     )
 }
