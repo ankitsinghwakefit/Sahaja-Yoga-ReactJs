339d24696b45e66300a96cab30fdf64cd96cf35d
ankit <ankitsinghwakefit@gmail.com>
2020-03-14 21:38:52 +0530

    responsive done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,6 @@ function Footer(){
         <div className="footer-container">
             <div className="footer-logo"></div>
             <div className="footer-tagline">Presented by the Shri Mataji Nirmala Devi Sahaja Yoga World Foundation</div>
-            <button class="button is-white">White</button>
         </div>
     )
 }
